b131e447a6bf5584b61c27d40e310b9ca7f292b0
Dallas Moore <moore.johndallas@gmail.com>
2023-05-15 22:38:37 -0500

    Added inquire questions

--- Develop/questions.js
+++ Develop/questions.js
@@ -1,7 +1,44 @@
 const questions = [
     {
         type: "input",
-        message: "What is your project name?",
-        name: "projectName"
+        message: "What is your project's title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please provide a description your project:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Please provide installation instructions:",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Explain how your project is meant to be used:",
+        name: "usage"
+    },
+    {
+        type: 'list',
+        message: "What type of license did you use?",
+        name: 'license',
+        choices: ['GNU', 'MIT', 'Apache']
+    },
+    {
+        type: "input",
+        message: "Please give credit as needed:",
+        name: "credits"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "github"
     }
+
 ]
